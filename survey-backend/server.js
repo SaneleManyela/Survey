685d28716b02635acea8d6b9f52e911cb8110fe3
SANELE MANYELA <63672476+SaneleManyela@users.noreply.github.com>
2025-10-22 19:50:49 +0200

    CORS Policy Update

--- survey-backend/server.js
+++ survey-backend/server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://sanelemanyela.github.io/Survey',
-  methods: ['GET', 'POST'],
+  origin: 'https://sanelemanyela.github.io',
+  methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
 
